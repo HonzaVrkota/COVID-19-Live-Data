c0e641ffbe6a122dc6e72888eed2a4df0fa91ba4
HonzaVrkota <46007571+HonzaVrkota@users.noreply.github.com>
2020-03-20 20:55:36 +0100

    Change version in desc

--- covid-19-live-data/covid-19-live-data.php
+++ covid-19-live-data/covid-19-live-data.php
@@ -16,7 +16,7 @@
  * Plugin Name:       COVID-19 Live Data
  * Plugin URI:        https://dameweb.eu
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Jan Vrkota
  * Author URI:        https://dameweb.eu
  * License:           GPL-2.0+
